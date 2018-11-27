9b61425616c29c4f7f7400952cad92b1d2a62352
Sergio Baray <Sergio@Sergios-MacBook-Pro-3.local>
2018-11-26 21:17:44 -0700

    Adding Updated Mac OS version

---
+++ Bonjour-Mac-OS/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  XClicker-1st-prototype
+//
+//  Created by Sergio Baray, Angela Kearns, Elena Davidson
+// 
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Bonjour-Mac-OS/bonjour-demo-mac-Bridging-Header.h
@@ -0,0 +1,5 @@
+//
+//  Using this file to import target's public headers that you would like to expose to Swift.
+//
+
+#import "GCDAsyncSocket.h"
